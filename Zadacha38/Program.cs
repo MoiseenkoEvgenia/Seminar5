95496e2f882fbaf82d2c59e9581c010d674ec52a
MoiseenkoEvgenia <sokolova-2206@mail.ru>
2022-11-18 18:20:45 +0300

    add Zadacha 38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+ {
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        numbers[j] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+    }
+ }
+ void PrintArray(double[] numbers)
+{
+    Console.Write("[");
+    for (int j = 0; j < numbers.Length; j++)
+    {
+       Console.Write(numbers[j] + ", "); 
+    }
+    Console.Write("\b\b");
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
